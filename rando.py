f0c4833701e1b7b4f4c267ab15f961ee4b14bf20
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2016-10-22 19:33:50 -0400

    Clean up card values/suits definition

--- rando.py
+++ rando.py
@@ -3,6 +3,51 @@ import random
 from errbot import botcmd
 from errbot import BotPlugin
 
+
+CARD_VALUES = [
+    'ace',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'jack',
+    'queen',
+    'king',
+]
+CARD_VALUES_NUM = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+]
+CARD_SUITS = [
+  'hearts',
+  'diamonds',
+  'clubs',
+  'spades',
+]
+CARD_SUITS_SYMBOLS = [
+    u'\u2665',  #heart
+    u'\u2666',  #diamonds
+    u'\u2663',  #clubs
+    u'\u2660',  #spades
+]
+
+
 class Rando(BotPlugin):
 
     @botcmd(admin_only=False)
@@ -18,15 +63,8 @@ class Rando(BotPlugin):
     @botcmd(admin_only=False)
     def dealcard(self, mess, args):
         """Returns a card: [ace..king] of [hearts|diamonds|clubs|spades]"""
-        value = random.choice([('two', '2'), ('three', '3'), ('four', '4'),
-                               ('five', '5'), ('six', '6'), ('seven', '7'),
-                               ('eight', '8'), ('nine', '9'), ('ten', '10'),
-                               ('jack', 'J'), ('queen', 'Q'), ('king', 'K'),
-                               ('ace', 'A')])
-        suit = random.choice([('hearts', u'\u2665'),
-                              ('diamonds', u'\u2666'),
-                              ('clubs', u'\u2663'),
-                              ('spades', u'\u2660')])
+        value = random.choice(zip(CARD_VALUES, CARD_VALUES_NUM))
+        suit = random.choice(zip(CARD_SUITS, CARD_SUITS_SYMBOLS))
         return u"{0} of {1} [{2}{3}]".format(value[0], suit[0], suit[1], value[1])
 
     @botcmd(split_args_with=' ', admin_only=False)
